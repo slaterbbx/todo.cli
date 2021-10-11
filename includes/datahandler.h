95cb17c2b0584ede31d8b57f21d30c57a19409e2
slaterbbx <litonfiredesign@gmail.com>
2021-10-10 20:48:00 -0500

    int project files commit

---
+++ includes/datahandler.h
@@ -0,0 +1,27 @@
+
+
+class DataHandler {
+
+public:
+
+	// function that checks file existance ( list.todo )
+		// * if no
+			// - create file / fill with any needed instructions or info
+		// * if yes
+			// return 1 to signify true and 0 for false ( file existance )
+
+	// function that parses list.todo to display it with proper spacing / alignment in terminal
+
+	// function that lists todo list items
+			// # NOTE: reads and std::cout each line
+			// # NOTE: file / text formatting handled by another "parsing" function
+
+	// function that adds todo list items
+
+	// function that removes todo list items
+
+	// function that handles moving ( cmd: 'mv' ) todo list items up / down
+
+	// function that opens list.todo file in default text editor for editing
+
+};
---
+++ src/jtdi.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : jtdi.cpp
+// Author      : vader::joker
+// Version     : 0.0.1
+// Copyright   : MIT
+// Description : to:do list CLI in C++
+//============================================================================
+
+#include <iostream>
+
+
+int main(int argc, char* argv[]) {
+	std::cout << "vader::joker" << std::endl; // prints vader::joker
+	return 0;
+}
