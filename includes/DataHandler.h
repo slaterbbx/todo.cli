5b6461cd878830f06f2e60de0c58e23891da5c24
slaterbbx <litonfiredesign@gmail.com>
2021-10-11 23:14:19 -0500

    added many notes about functionality

--- includes/DataHandler.h
+++ includes/DataHandler.h
@@ -5,7 +5,10 @@ class DataHandler {
 public:
 
 	// this function will handle initializing a git project, commiting / pushing and pulling
+	// should use GIT CLI and GITHUB CLI together to log in and manage git project easily
 	void GitConnect();
+
+	// ------------------------------------------------
 
 	// function that checks file existance ( list.todo )
 		// * if no
@@ -16,15 +19,14 @@ public:
 	bool FileAuthenticator(const char*); // COMPLETED FUNCTIONALITY
 	void CreateFile(const char*); // SELF EXPLANITORY
 
-
 	// ------------------------------------------------
 
-	// function that parses lists.todo to display it with proper spacing / alignment in terminal ( default option )
-	// by default, on startup of app, parser will clean the file, incase of out of app edits. ie, delete spaces / empty lines
-	// function that checks off todo list items
-	// function that clears checked off items
-	// function that archives checked off items with date / timestamp in file
-	// function that clears archive file and todo list file
+	// |-| function that parses lists.todo to display it with proper spacing / alignment in terminal ( default option )
+	// |-| by default, on startup of app, parser will clean the file, incase of out of app edits. ie, delete spaces / empty lines
+	// |-| function that checks off todo list items
+	// |-| function that clears checked off items
+	// |-| function that archives checked off items with date / timestamp in file
+	// |-| function that clears archive file and todo list file
 
 		// NOTE: after a certain amount of time or quantity, divide "archive lists" into sep files in folder /archives/
 		// |-| add command to purge archives based on date ranges
@@ -34,7 +36,21 @@ public:
 	// |-| FEATURE auto divides grouped lists into folders / files
 	// |-| IMPORTANT uses git services like github to manage lists that GUI versions also can sync to
 
-	bool DataManager(); // NOTE: this will probably be broken out into many smaller methods or a subclass altogether
+	// ### NEW LIST BASED ON FEATURE PLANS
+
+
+	bool FileManager(); // NOTE: this will probably be broken out into many smaller methods or a subclass altogether
+
+	// ------------------------------------------------
+
+	// Init project for TODO that sets up initial folder structure and files for local todo list tracking
+	// We are using files / folder so that our lists can be organized into a project and incorporated into git tracking -
+	// we can use a "global" file set by default ( system ) / project by project basis when in a project folder
+	// NOTE: set an "override" for the in project folder scoping so that you can switch between projects from anywhere -
+	// FEATURE add option to omit TODO code from files when posting to git by passing git push through TODO app
+	// -- Makes a copy of the project / deletes comments related to TODO pushs through todo to git the copy and deletes it
+
+	void TodoInit();
 
 	// ------------------------------------------------
 
