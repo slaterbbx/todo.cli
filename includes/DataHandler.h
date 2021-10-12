ca679ac0fac303f9a0cc7806fcd3f55ed4cda593
slaterbbx <litonfiredesign@gmail.com>
2021-10-12 01:22:38 -0500

    added comments

--- includes/DataHandler.h
+++ includes/DataHandler.h
@@ -5,7 +5,7 @@ class DataHandler {
 public:
 
 	// this function will handle initializing a git project, commiting / pushing and pulling
-	// -- this is used for local system versions, regular git is for project TODO lists
+	// -- this is used for local system todo lists, regular git is for project TODO lists
 	// should use GIT CLI and GITHUB CLI together to log in and manage git project easily
 	void GitConnect();
 
@@ -23,10 +23,21 @@ public:
 
 	// ------------------------------------------------
 
-	// |-| function that parses lists.todo to display it with proper spacing / alignment in terminal ( default option )
+	// |#| DataHandler.h // because this "group" title matches file name, this will be a global wrapper -
+	// around the entire files todo list. ( auto puts all other # markers as sub categories )
+	// |-| global to the DataHandler.h file
+	// |#| FileManager() // displays as "group" title for todo items
+	// |-| specific to the FileManager() class method of DataHandler.h file and will be organized -
+	// accordingly when displayed in terminal with all following tasks / todos as sub items. -
+	// this will happen until the next occurrence of |#|
+
+	// ## THE FOLLOWING WILL BE USED TO TEST THE TODO FILE WATCHING
+
+	// |-| function that parses lists.todo to handle project todo files ( list of folder / file, addresses )
 	// |-| by default, on startup of app, parser will clean the file, incase of out of app edits. ie, delete spaces / empty lines
 	// |-| function that checks off todo list items
-	// |-| function that clears checked off items
+	// |x| 1A4L | function that clears checked off items ( Kyle Gallagher | Front end design | 8/29/2021 ) -
+	// continue the last line
 	// |-| function that archives checked off items with date / timestamp in file
 	// |-| function that clears archive file and todo list file
 
@@ -37,6 +48,8 @@ public:
 	// |-| FEATURE add feature to handle projects ( programming / files ) with todo syntax in them ( using comment systems )
 	// |-| FEATURE auto divides grouped lists into folders / files
 	// |-| IMPORTANT uses git services like github to manage lists that GUI versions also can sync to
+
+	// |#| FileManager().end
 
 	// ### NEW LIST BASED ON FEATURE PLANS
 
