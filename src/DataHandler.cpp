0a01c96ca77eb15d84c2fed8bd42e3a9fe6300d7
slaterbbx <litonfiredesign@gmail.com>
2021-10-11 20:51:05 -0500

    minor changes

--- src/DataHandler.cpp
+++ src/DataHandler.cpp
@@ -8,10 +8,12 @@ bool DataHandler::FileAuthenticator(const char* fileName){
 	if(std::filesystem::exists(fileName)){
 			std::cout << "File " << fileName << " exists" << std::endl;
 			return true;
-		}
-
-	std::ofstream createFile (fileName);
-	std::cout << "program file created on system '" << fileName << "'" << std::endl;
-	createFile.close();
+	};
 	return false;
-}
+};
+
+void DataHandler::CreateFile(const char* fileName){
+	std::cout << "program file created on system '" << fileName << "'" << std::endl;
+	std::ofstream createFile (fileName);
+	createFile.close();
+};
