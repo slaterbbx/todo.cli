9d6d7f9e99993cf296b782235a42b26a46ec52b7
slaterbbx <litonfiredesign@gmail.com>
2021-10-12 17:17:32 -0500

    cleaned comments

--- includes/DataHandler.h
+++ includes/DataHandler.h
@@ -8,70 +8,15 @@ class DataHandler {
 
 public:
 
-	// this function will handle initializing a git project, commiting / pushing and pulling
-	// -- this is used for local system todo lists, regular git is for project TODO lists
-	// should use GIT CLI and GITHUB CLI together to log in and manage git project easily
+	// method that uses GIT CLI and GH CLI to push non project based todo lists to git
 	void GitConnect();
-
-	// ------------------------------------------------
-
-	// function that checks file existance ( list.todo )
-		// * if no
-			// - return false
-		// * if yes
-			// return true
-
-	bool FileAuthenticator(const char*); // COMPLETED FUNCTIONALITY
-
-	void CreateFile(const char*); // SELF EXPLANITORY
-
-	// ------------------------------------------------
-
-	// |#| DataHandler.h // because this "group" title matches file name, this will be a global wrapper -
-	// around the entire files todo list. ( auto puts all other # markers as sub categories )
-	// |-| global to the DataHandler.h file
-	// |#| FileManager() // displays as "group" title for todo items
-	// |-| specific to the FileManager() class method of DataHandler.h file and will be organized -
-	// accordingly when displayed in terminal with all following tasks / todos as sub items. -
-	// this will happen until the next occurrence of |#|
-
-	// ## THE FOLLOWING WILL BE USED TO TEST THE TODO FILE WATCHING
-
-	// |-| function that parses lists.todo to handle project todo files ( list of folder / file, addresses )
-	// |-| by default, on startup of app, parser will clean the file, incase of out of app edits. ie, delete spaces / empty lines
-	// |-| function that checks off todo list items
-	// |x| 1A4L | function that clears checked off items ( Kyle Gallagher | Front end design | 8/29/2021 ) -
-	// continue the last line
-	// |-| function that archives checked off items with date / timestamp in file
-	// |-| function that clears archive file and todo list file ( global )
-
-		// NOTE: after a certain amount of time or quantity, divide "archive lists" into sep files in folder /archives/ ( global )
-		// |-| add command to purge archives based on date ranges
-		// |-| add command to allow users to turn on or off archiving ( on with 1 month limit by default )
-
-	// |-| FEATURE add feature to handle projects ( programming / files ) with todo syntax in them ( using comment systems )
-	// |-| FEATURE auto divides grouped lists into folders / files
-	// |-| IMPORTANT uses git services like github to manage lists that GUI versions also can sync to
-
-	// |#| FileManager().end
-
-	// ### NEW LIST BASED ON FEATURE PLANS
-
+	// method that checks file existance ( list.todo )
+	bool FileAuthenticator(const char*);
+	// method that creates files
+	void CreateFile(const char*);
+	// method that creates and deletes files ( includes file authentication )
 	void FileManager(int, const char*);
-
-	// ------------------------------------------------
-
-	// Init project for TODO that sets up initial folder structure and files for local todo list tracking
-	// NOTE: set an "override" for the in project folder scoping so that you can switch between projects from anywhere -
-	// FEATURE add option to omit TODO code from files when posting to git by passing git push through TODO app
-	// -- Makes a copy of the project / deletes comments related to TODO pushs through todo to git the copy and deletes it
-
-	// Make Directory's and Files
-	// Set innitial .config
-		// sets defaults
-		// later holds data to connect with backend
-
-
+	// method to init todoit project
 	void TodoInit();
 
 	// ------------------------------------------------
@@ -82,23 +27,11 @@ public:
 	// |-| function that takes commands for date ranges to show archived todo items ( date range / specific / or all archives )
 
 	void ListItems(); // ( -ls ) or ( -ls [1] ) "[] means optional"
-
-	// ------------------------------------------------
-
-	// function that adds todo list items
-
+	// method that adds todo list items
 	void AddItem();
-
-	// ------------------------------------------------
-
-	// function that removes todo list items
-
+	// method that removes todo list items
 	void RemoveItem();
-
-	// ------------------------------------------------
-
-	// function that handles moving ( cmd: 'mv' ) todo list items up / down
-
+	// method that handles moving ( cmd: 'mv' ) todo list items up / down
 	void MoveItem();
 
 	// ------------------------------------------------
@@ -107,10 +40,39 @@ public:
 			// - additional commands might be (
 		// OR edit an individual item by re-writing it ( todo -e 1 "then new string, todo item" )
 			// - EXAMPLE ^ : todo -e 2 warm up a cup of coffee
-
 	void Edit();
 
 };
+
+// ------------------------------------------------
+
+// |#| DataHandler.h // because this "group" title matches file name, this will be a global wrapper -
+// around the entire files todo list. ( auto puts all other # markers as sub categories )
+// |-| global to the DataHandler.h file
+// |#| FileManager() // displays as "group" title for todo items
+// |-| specific to the FileManager() class method of DataHandler.h file and will be organized -
+// accordingly when displayed in terminal with all following tasks / todos as sub items. -
+// this will happen until the next occurrence of |#|
+
+// ## THE FOLLOWING WILL BE USED TO TEST THE TODO FILE WATCHING
+
+// |-| function that parses lists.todo to handle project todo files ( list of folder / file, addresses )
+// |-| by default, on startup of app, parser will clean the file, incase of out of app edits. ie, delete spaces / empty lines
+// |-| function that checks off todo list items
+// |x| 1A4L | function that clears checked off items ( Kyle Gallagher | Front end design | 8/29/2021 ) -
+// continue the last line
+// |-| function that archives checked off items with date / timestamp in file
+// |-| function that clears archive file and todo list file ( global )
+
+	// NOTE: after a certain amount of time or quantity, divide "archive lists" into sep files in folder /archives/ ( global )
+	// |-| add command to purge archives based on date ranges
+	// |-| add command to allow users to turn on or off archiving ( on with 1 month limit by default )
+
+// |-| FEATURE add feature to handle projects ( programming / files ) with todo syntax in them ( using comment systems )
+// |-| FEATURE auto divides grouped lists into folders / files
+// |-| IMPORTANT uses git services like github to manage lists that GUI versions also can sync to
+
+// ------------------------------------------------
 
 // ### SOME THOUGHTS FOR GUI VERSION ### ///
 //
