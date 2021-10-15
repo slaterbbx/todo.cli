d6455fd4463d34b7c4a811677a24899e5bc5e36d
slaterbbx <litonfiredesign@gmail.com>
2021-10-15 00:34:16 -0500

    updated header

--- src/todo.cpp
+++ src/todo.cpp
@@ -1,10 +1,10 @@
 //============================================================================
 // Name        : Just TODO it
-// Author      : vader::joker
+// Author      : @vader.iam::joker::slaterbbx
 // Version     : 0.0.1
 // Description : todo list CLI in C++
+// License : MIT
 // inotifywait -m -e CLOSE_WRITE --format="Debug/todo" Debug/ | sh
-//
 //============================================================================
 
 #include <iostream>
@@ -14,8 +14,6 @@ FileManager FileManager;
 
 int main(int argc, char* argv[]) {
 
-//	FileManager.CreateDirectory("testingThings");
-//	FileManager.CreateDirectory("testingThings");
 	FileManager.Init();
 
 	return 0;
